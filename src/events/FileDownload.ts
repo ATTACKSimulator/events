04de092e1add8ac5cccf601ad32fa94d9f90b44c
Dan Florian <dan@attacksimulator.com>
2023-08-03 12:26:08 +0300

    Added new events: button_clicked, file_download and page_left

---
+++ src/events/FileDownload.ts
@@ -0,0 +1,54 @@
+import IEvent from "../intefaces/IEvent";
+import ATSEvent from "./ATSEvent";
+
+export default class FileDownload extends ATSEvent implements IEvent {
+	get shouldDebounce(): boolean {
+		return false;
+	}
+	get trigger(): null {
+		return null;
+	}
+	get source(): null {
+		return null;
+	}
+	get name(): string {
+		return "file_download";
+	}
+	get hasTypes(): boolean {
+		return true;
+	}
+	get redirectOnFinish(): boolean {
+		return false;
+	}
+	get isBlocking(): boolean {
+		return false;
+	}	
+	get allowMultiple(): boolean {
+		return false;
+	}
+
+	validate(event: Event): boolean {
+		if (event?.target instanceof HTMLAnchorElement) {
+			return this.validateAnchor(event.target);
+		}
+		
+
+		if (event?.target instanceof HTMLButtonElement) {
+			return this.validateButton(event.target);
+		}
+
+		return false;
+	}
+
+	private validateAnchor(element: HTMLElement): boolean {
+		return this.basicValidation(element);
+	}
+
+	private validateButton(element: HTMLElement): boolean {
+		if (!this.basicValidation(element) || (element.hasAttribute("type") && element.getAttribute("type") === "submit")) {
+			return false;
+		}
+			
+		return true;
+	}
+} 
---
+++ src/events/PageLeft.ts
@@ -0,0 +1,32 @@
+import IEvent from "../intefaces/IEvent";
+import ATSEvent from "./ATSEvent";
+
+export default class PageLoaded extends ATSEvent implements IEvent {
+	get shouldDebounce(): boolean {
+		return false;
+	}
+	get trigger(): null {
+		return null;
+	}
+	get source(): Document {
+		return document;
+	}
+	get name(): string {
+		return "page_left";
+	}
+	get hasTypes(): boolean {
+		return false;
+	}
+	get redirectOnFinish(): boolean {
+		return false;
+	}
+	get isBlocking(): boolean {
+		return false;
+	}
+	get allowMultiple(): boolean {
+		return false;
+	}
+	validate(): boolean {
+		return document.visibilityState === "hidden";
+	}
+}
