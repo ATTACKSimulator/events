80ab1dccc19ecda04b998b156c1680858d87fd37
Danut Florian <dan@attacksimulator.com>
2024-12-28 02:47:08 +0200

    Better @click detection (support for .stop and .prevent modifiers)

--- src/events/ATSEvent.ts
+++ src/events/ATSEvent.ts
@@ -3,9 +3,18 @@ export default class ATSEvent {
 		if (element.hasAttribute("data-ignore")) {
 			return false;
 		}
-		if (element.hasAttribute("@click")) {
-			return false;
+
+		// check if the element has @click with any of the .stop or .prevent modifiers
+		for (const attribute of element.attributes) {
+			// check if attribute starts with @click
+			if (attribute.name.startsWith("@click")) {
+				// check if the attribute contains .stop or .prevent
+				if (attribute.name.includes(".stop") || attribute.name.includes(".prevent")) {
+					return false;
+				}
+			}
 		}
+
 		return true;
 	}
 }
